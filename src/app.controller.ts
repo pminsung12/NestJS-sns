38b0deafc549ac52eef44e7d628e1c5f14c631b6
Minseong Park <pminsung12@gmail.com>
2023-11-01 02:30:09 +0900

    feat: getPosts, getPost 컨트롤러 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-/**
- * author: string;
- * title: string;
- * content: string;
- * likeCount: number;
- * commentCount: number;
- * */
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      con,
-    };
-  }
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,64 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ * */
+
+interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+const posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'John Doe',
+    title: 'Hello World',
+    content: 'Hello world from John Doe',
+    likeCount: 10,
+    commentCount: 2,
+  },
+  {
+    id: 2,
+    author: 'Sally Kim',
+    title: 'Hello World',
+    content: 'Hello world from Sally Kim',
+    likeCount: 13,
+    commentCount: 15,
+  },
+  {
+    id: 3,
+    author: 'Banana',
+    title: 'Hello World',
+    content: 'Hello world from Banana',
+    likeCount: 92,
+    commentCount: 34,
+  },
+];
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPosts(): PostModel[] {
+    return posts;
+  }
+
+  @Get(':id')
+  getPost(@Param('id') id: string): PostModel {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException('The post is not found');
+    }
+    return post;
+  }
 }
