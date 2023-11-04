d42b2ce3f6bba0676ae0e46eca3ce9a5af8cd6e3
Minseong Park <pminsung12@gmail.com>
2023-11-05 03:25:06 +0900

    feat: jwt 토큰을 Authorization 헤더로 인증하는 방식으로 변경

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -19,6 +19,31 @@ export class AuthService {
    * 2) sub -> id
    * 3) type : 'access | refresh'
    */
+
+  extractTokenFromHeader(header: string, isBearer: boolean) {
+    const splitToken = header.split(' ');
+    const prefix = isBearer ? 'Bearer' : 'Basic';
+
+    if (splitToken.length !== 2 || splitToken[0] !== prefix) {
+      throw new UnauthorizedException('잘못된 토큰입니다.');
+    }
+
+    const token = splitToken[1];
+    return token;
+  }
+
+  decodeBasicToken(base64String: string) {
+    const decoded = Buffer.from(base64String, 'base64').toString('utf8');
+    const split = decoded.split(':');
+    if (split.length !== 2) {
+      throw new UnauthorizedException('잘못된 유형의 토큰입니다.');
+    }
+    const email = split[0];
+    const password = split[1];
+
+    return { email, password };
+  }
+
   signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
     const payload = {
       email: user.email,
