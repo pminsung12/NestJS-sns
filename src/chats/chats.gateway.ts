df93e92babe6f31f4174299626e7ee79aaec7cb8
Minseong Park <pminsung12@gmail.com>
2023-11-13 18:42:30 +0900

    feat: join을 이용한 채팅룸 생성, socket.to를 이용한 broadcasting 구현

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,22 +1,45 @@
 import {
+  ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   // ws://localhost:3000/chats
   namespace: 'chats',
 })
 export class ChatsGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Server;
+
   handleConnection(socket: Socket): any {
     console.log(`on connect called: ${socket.id}`);
   }
+
+  @SubscribeMessage('enter_chat')
+  enterChat(
+    // 방의 chat ID들을 리스트로 받는다.
+    @MessageBody() data: number[],
+    @ConnectedSocket() socket: Socket,
+  ) {
+    for (const chatId of data) {
+      socket.join(chatId.toString());
+    }
+  }
   // socket.on('send_message', (message) => { console.log(message); });
   @SubscribeMessage('send_message')
-  sendMessage(@MessageBody() message: string): void {
-    console.log(message);
+  sendMessage(
+    @MessageBody() message: { message: string; chatId: number },
+    @ConnectedSocket() socket: Socket,
+  ): void {
+    // this.server는 나한테도 옴, socket.to는 나를 제외한 broadcasting
+    // this.server
+    //   .in(message.chatId.toString())
+    //   .emit('receive_message', 'hello from server');
+    socket.to(message.toString()).emit('receive_message', message.message);
   }
 }
